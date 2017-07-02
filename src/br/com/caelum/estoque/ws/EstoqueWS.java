820b3812cca84c426fa2b650ca78987be3593d39
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-07-02 11:37:04 -0300

    C3M2.10 - RequestWrapper

--- src/br/com/caelum/estoque/ws/EstoqueWS.java
+++ src/br/com/caelum/estoque/ws/EstoqueWS.java
@@ -3,12 +3,17 @@ package br.com.caelum.estoque.ws;
 import java.util.List;
 
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.xml.ws.RequestWrapper;
 import javax.xml.ws.ResponseWrapper;
 
+import br.com.caelum.estoque.modelo.item.Filtro;
+import br.com.caelum.estoque.modelo.item.Filtros;
 import br.com.caelum.estoque.modelo.item.Item;
 import br.com.caelum.estoque.modelo.item.ItemDao;
+import br.com.caelum.estoque.modelo.item.ListaItens;
 
 @WebService
 public class EstoqueWS {
@@ -18,9 +23,11 @@ public class EstoqueWS {
 	@WebMethod(operationName="todosOsItens")
 	@ResponseWrapper(localName="itens")
 	@WebResult(name="itens")
-	public List<Item> getItens() { 
-		
-	    System.out.println("Chamando getItens()");
-	    return dao.todosItens();
+	@RequestWrapper(localName="listaItens")
+	public ListaItens getItens(@WebParam(name="filtros") Filtros filtros){
+
+        List<Filtro> lista = filtros.getLista();
+        List<Item> result = dao.todosItens(lista);
+        return new ListaItens(result);
 	}
 }
