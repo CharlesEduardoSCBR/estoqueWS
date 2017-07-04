09b44ad42aed26cd759037bef472497df4c9048c
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-07-03 23:05:01 -0300

    C3M3.06 - Autorizacao pelo Token

--- src/br/com/caelum/estoque/ws/EstoqueWS.java
+++ src/br/com/caelum/estoque/ws/EstoqueWS.java
@@ -8,12 +8,14 @@ import javax.jws.WebResult;
 import javax.jws.WebService;
 import javax.xml.ws.RequestWrapper;
 import javax.xml.ws.ResponseWrapper;
+import javax.xml.ws.WebFault;
 
 import br.com.caelum.estoque.modelo.item.Filtro;
 import br.com.caelum.estoque.modelo.item.Filtros;
 import br.com.caelum.estoque.modelo.item.Item;
 import br.com.caelum.estoque.modelo.item.ItemDao;
 import br.com.caelum.estoque.modelo.item.ListaItens;
+import br.com.caelum.estoque.modelo.usuario.AutorizacaoException;
 import br.com.caelum.estoque.modelo.usuario.TokenDao;
 import br.com.caelum.estoque.modelo.usuario.TokenUsuario;
 
@@ -37,10 +39,15 @@ public class EstoqueWS {
 	@WebResult(name = "item")
 	public Item cadastrarItem(
 			@WebParam(name = "tokenUsuario", header = true) TokenUsuario token,
-			@WebParam(name = "item") Item item) {
+			@WebParam(name = "item") Item item) 
+					throws AutorizacaoException {
 		
-		System.out.println("Cadastrando " + item);
+		System.out.println("Cadastrando " + item + "/ Token" + token);
 		boolean valido = new TokenDao().ehValido(token);
+		
+		if(!valido){
+			throw new AutorizacaoException("Autorizacao falhou");
+		}
 		this.dao.cadastrar(item);
 		
 		return item;
