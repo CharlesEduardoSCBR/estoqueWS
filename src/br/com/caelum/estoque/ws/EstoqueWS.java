2881f0de6e2a0875859075c0716fabc1ba087e13
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-07-03 22:54:19 -0300

    C3M3.05 - Usando o Header

--- src/br/com/caelum/estoque/ws/EstoqueWS.java
+++ src/br/com/caelum/estoque/ws/EstoqueWS.java
@@ -14,20 +14,35 @@ import br.com.caelum.estoque.modelo.item.Filtros;
 import br.com.caelum.estoque.modelo.item.Item;
 import br.com.caelum.estoque.modelo.item.ItemDao;
 import br.com.caelum.estoque.modelo.item.ListaItens;
+import br.com.caelum.estoque.modelo.usuario.TokenDao;
+import br.com.caelum.estoque.modelo.usuario.TokenUsuario;
 
 @WebService
 public class EstoqueWS {
 
 	private ItemDao dao = new ItemDao();
 
-	@WebMethod(operationName="todosOsItens")
-	@ResponseWrapper(localName="itens")
-	@WebResult(name="itens")
-	@RequestWrapper(localName="listaItens")
-	public ListaItens getItens(@WebParam(name="filtros") Filtros filtros){
+	@WebMethod(operationName = "todosOsItens")
+	@ResponseWrapper(localName = "itens")
+	@WebResult(name = "itens")
+	@RequestWrapper(localName = "listaItens")
+	public ListaItens getItens(@WebParam(name = "filtros") Filtros filtros) {
 
-        List<Filtro> lista = filtros.getLista();
-        List<Item> result = dao.todosItens(lista);
-        return new ListaItens(result);
+		List<Filtro> lista = filtros.getLista();
+		List<Item> result = dao.todosItens(lista);
+		return new ListaItens(result);
+	}
+
+	@WebMethod(operationName = "CadastrarItem")
+	@WebResult(name = "item")
+	public Item cadastrarItem(
+			@WebParam(name = "tokenUsuario", header = true) TokenUsuario token,
+			@WebParam(name = "item") Item item) {
+		
+		System.out.println("Cadastrando " + item);
+		boolean valido = new TokenDao().ehValido(token);
+		this.dao.cadastrar(item);
+		
+		return item;
 	}
 }
