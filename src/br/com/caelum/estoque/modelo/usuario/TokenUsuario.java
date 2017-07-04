4689da54c896888572969cca500b30fc3b3d4be8
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-07-04 19:55:06 -0300

    C3M3.11 - (Desafio) XmlAdapter

--- src/br/com/caelum/estoque/modelo/usuario/TokenUsuario.java
+++ src/br/com/caelum/estoque/modelo/usuario/TokenUsuario.java
@@ -6,6 +6,7 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
@@ -14,6 +15,7 @@ public class TokenUsuario {
 	@XmlElement(required=true)
 	private String token;
 	
+	@XmlJavaTypeAdapter(DateAdapter.class)
 	@XmlElement(required=true)
 	private Date dataValidade;
 	
